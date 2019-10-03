7ec7e42e881356c6c0695a006e3927bb750fff9e
kuwas-h <kuwas-h@users.noreply.github.com>
2019-10-03 17:25:27 +0900

    Update changecolor.js

--- script/changecolor.js
+++ script/changecolor.js
@@ -3,6 +3,7 @@ const greeting = document.querySelector('.greeting');
 const colorbutton = document.querySelector('.button-list button');
 colorbutton.addEventListener("click", changecolor);
 function changecolor(event){
+  out(event.target.innerHTML);
   const color = event.target.value;
   out(color);
   elem.style.setProperty('--main-color', color);
